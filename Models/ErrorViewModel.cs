ad6601373714fb8d1d8325867584bd2f8c68084a
Hugo da Silva Matos <hugomatos14@gmail.com>
2026-03-25 17:55:20 -0300

    testa ai joaozinho

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace RetroagirEntradas.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
